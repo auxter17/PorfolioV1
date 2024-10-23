aac9c2e4201eaf16536a5d08afa2b2a5af7d7a63
gelo1725 <angelo.auxtero@wela.online>
2024-10-23 13:31:32 +0800

    update

---
+++ src/components/menu.svelte
@@ -0,0 +1,71 @@
+<script>
+	import menuIcon from '/src/images/menu-2.svg';
+	import closeIcon from '/src/images/closesvg.svg';
+
+	let menuOpen = true;
+	function handleMenu() {
+		menuOpen = !menuOpen;
+	}
+	const menus = [
+		{ name: 'Home', link: '/', active: '/' },
+		{ name: 'About Me', link: '/aboutMe', active: 'aboutMe' },
+		{ name: 'Services', link: '/service', active: 'service' },
+		{ name: 'Portfolio', link: '/portfolio', active: 'portfolio' },
+		{ name: 'Contact Me', link: '/contactMe', active: 'contactMe' }
+	];
+</script>
+
+<section class="dropdown md:hidden">
+	<button
+		class="border-2 m-2 hover:bg-gray-900 rounded flex ml-[65px] border-[#70FDBB] bg-zinc-700 p-2"
+		on:click={handleMenu}
+	>
+		{#if menuOpen}
+			<img
+				class="w-[35px] transition-transform duration-500"
+				src={closeIcon}
+				alt="close"
+				style="transform: rotate(0deg);"
+			/>
+		{:else}
+			<img
+				class="w-[35px] transition-transform duration-500"
+				src={menuIcon}
+				alt="menu"
+				style="transform: rotate(180deg);"
+			/>
+		{/if}
+	</button>
+
+	<div
+		id="myDropdown"
+		class:show={menuOpen}
+		class="dropdown-content mr-2 bg-zinc-700 justify-start rounded flex flex-col border-2 border-[#70FDBB]"
+	>
+		{#if menus.length > 0}
+			{#each menus as menu}
+				<a class="text-white hover:bg-gray-900" href={menu.link}>{menu.name}</a>
+			{/each}
+		{/if}
+	</div>
+</section>
+
+<style>
+	.dropdown-content {
+		display: none;
+		flex-direction: column;
+		z-index: 1;
+	}
+	.show {
+		display: flex;
+	}
+	.dropdown-content a {
+		padding: 8px 16px;
+		text-decoration: none;
+		display: block;
+	}
+
+	.dropdown button {
+		cursor: pointer;
+	}
+</style>
--- src/components/navBar.svelte
+++ src/components/navBar.svelte
@@ -2,15 +2,20 @@
 	import { slide, fly } from 'svelte/transition';
 	import miniIcon from '/src/images/icon.png';
 	import LoadingMain from './loadingscreens/loadingMain.svelte';
+	import Menu from './menu.svelte';
 
 	let active = '';
 	let show = true;
 
-	/**
-	 * @param {string} link
-	 */
-	function setActive(link) {
-		active = link;
+	const menus = [
+		{ name: 'Home', link: '/', active: '/' },
+		{ name: 'About Me', link: '/aboutMe', active: 'aboutMe' },
+		{ name: 'Services', link: '/service', active: 'service' },
+		{ name: 'Portfolio', link: '/portfolio', active: 'portfolio' }
+	];
+
+	function setActive(name) {
+		active = name;
 	}
 
 	function handleClick() {
@@ -25,60 +30,39 @@
 
 <LoadingMain />
 
-<div class="absolute z-10 w-full font-bold">
+<div class="absolute z-20 w-full font-bold">
 	<nav class="flex justify-between p-8">
 		<ul class="flex m-0 justify-start">
 			<li>
 				<a on:click={handleClick} transition:slide class="hover:border-white" href="/">
 					{#if show}
-						<img transition:fly={{ x: -200 }} class="bg-white rounded-full" src={miniIcon} alt="" />
+						<img
+							transition:fly={{ x: -200 }}
+							class="bg-white rounded-full"
+							src={miniIcon}
+							alt="Mini Icon"
+						/>
 					{/if}
 				</a>
 			</li>
 		</ul>
-		<ul class="flex gap-x-16 px-12 py-4 m-0 justify-end">
-			<li>
-				<a
-					href="/"
-					on:click={() => setActive('/')}
-					class="hover:underline hover:underline-offset-8 cursor-pointer hover:text-[#70FDBB] transition duration-900 {active ===
-					'/'
-						? 'text-[#70FDBB]'
-						: ''}">Home</a
-				>
-			</li>
-			<li>
-				<a
-					href="/aboutMe"
-					on:click={() => setActive('aboutMe')}
-					class="hover:underline hover:underline-offset-8 cursor-pointer hover:text-[#70FDBB] transition duration-900 {active ===
-					'aboutMe'
-						? 'text-[#70FDBB]'
-						: ''}">About Me</a
-				>
-			</li>
-			<li>
-				<a
-					href="/service"
-					on:click={() => setActive('service')}
-					class="hover:underline hover:underline-offset-8 cursor-pointer hover:text-[#70FDBB] transition duration-900 {active ===
-					'service'
-						? 'text-[#70FDBB]'
-						: ''}">Services</a
-				>
-			</li>
-			<li>
-				<a
-					href="/portfolio"
-					on:click={() => setActive('portfolio')}
-					class="hover:underline hover:underline-offset-8 cursor-pointer hover:text-[#70FDBB] transition duration-900 {active ===
-					'portfolio'
-						? 'text-[#70FDBB]'
-						: ''}"
-				>
-					Portfolio
-				</a>
-			</li>
+		<ul class="hidden md:flex gap-x-16 px-12 py-4 m-0 justify-end">
+			{#if menus.length > 0}
+				{#each menus as menu}
+					<li>
+						<a
+							href={menu.link}
+							on:click={() => setActive(menu.active)}
+							class="hover:underline hover:underline-offset-8 cursor-pointer hover:text-[#70FDBB] transition duration-900 {active ===
+							menu.active
+								? 'text-[#70FDBB]'
+								: ''}"
+						>
+							{menu.name}
+						</a>
+					</li>
+				{/each}
+			{/if}
 			<li>
 				<a
 					href="/contactMe"
@@ -90,5 +74,9 @@
 				>
 			</li>
 		</ul>
+
+		<div>
+			<Menu />
+		</div>
 	</nav>
 </div>
